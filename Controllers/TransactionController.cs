9806fdd621acff58ad43e32825bf10f3aaba3aec
Duc <s3952670@student.rmit.edu.au>
2023-01-17 18:17:56 +1100

    Completed TransactionController

--- Controllers/TransactionController.cs
+++ Controllers/TransactionController.cs
@@ -3,6 +3,8 @@ using Assignment2.Data;
 using Assignment2.Filter;
 using Assignment2.Models;
 using Assignment2.Utilities;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+
 namespace Assignment2.Controllers;
 public class TransactionController : Controller
     {
@@ -27,19 +29,55 @@ public class TransactionController : Controller
             return View(account);
         }
 
-        // Note this code could be moved out of the controller, e.g., into the Model.
-        account.Balance += amount;
-        account.Transactions.Add(
-            new Transaction
-            {
-                TransactionType = "D",
-                Amount = amount,
-                TransactionTimeUtc = DateTime.UtcNow
-            });
+        LogTransaction(account, amount, "D");
 
         await _context.SaveChangesAsync();
 
-        return RedirectToAction(nameof(Index));
+        return RedirectToAction("Index", "Customer");
     }
+
+    public async Task<IActionResult> Widthdraw(int id) => View(await _context.Accounts.FindAsync(id));
+
+    [HttpPost]
+    public async Task<IActionResult> Widthdraw(int id, decimal amount)
+    {
+        var account = await _context.Accounts.FindAsync(id);
+
+        if (amount <= 0)
+            ModelState.AddModelError(nameof(amount), "Amount must be positive.");
+        if (amount.HasMoreThanTwoDecimalPlaces())
+            ModelState.AddModelError(nameof(amount), "Amount cannot have more than 2 decimal places.");
+        if (!ModelState.IsValid)
+        {
+            ViewBag.Amount = amount;
+            return View(account);
+        }
+
+        LogTransaction(account, -amount, "W");
+        if (account.Balance < 0)
+        {
+            ModelState.AddModelError(nameof(amount), "Insuffcient funds");
+            return View(account);
+        }
+        await _context.SaveChangesAsync();
+
+        return RedirectToAction("Index", "Customer");
+    }
+
+    private void LogTransaction(Account account, decimal amount, string type)
+    {   
+
+        account.Balance += amount;
+
+        account.Transactions.Add(
+            new Transaction
+            {
+                TransactionType = type,
+                Amount = amount,
+                TransactionTimeUtc = DateTime.UtcNow
+            });
+        
+    }
+
 
 }
