b26f523a9aa8516a7d1c333ec0b33c272b246b6e
Duc Vu <s3952670@student.rmit.edu.au>
2023-01-18 16:26:25 +1100

    added menu of actions for in the Customer => menu view

--- Controllers/CustomerController.cs
+++ Controllers/CustomerController.cs
@@ -16,14 +16,14 @@ public class CustomerController : Controller
 
     public CustomerController(McbaContext context) => _context = context;
 
-    // Can add authorize attribute to actions.
+    private Customer _customer;
     //[AuthorizeCustomer]
     public async Task<IActionResult> Index()
     {
         // Lazy loading.
         // The Customer.Accounts property will be lazy loaded upon demand.
         var customer = await _context.Customers.FindAsync(CustomerID);
-
+        _customer = customer;
         // OR
         // Eager loading.
         //var customer = await _context.Customers.Include(x => x.Accounts).
@@ -32,35 +32,7 @@ public class CustomerController : Controller
         return View(customer);
     }
 
-    //public async Task<IActionResult> Deposit(int id) => View(await _context.Accounts.FindAsync(id));
+    public async Task<IActionResult> Menu(int id) => View(await _context.Accounts.FindAsync(id));
 
-    //[HttpPost]
-    //public async Task<IActionResult> Deposit(int id, decimal amount)
-    //{
-    //    var account = await _context.Accounts.FindAsync(id);
 
-    //    if (amount <= 0)
-    //        ModelState.AddModelError(nameof(amount), "Amount must be positive.");
-    //    if (amount.HasMoreThanTwoDecimalPlaces())
-    //        ModelState.AddModelError(nameof(amount), "Amount cannot have more than 2 decimal places.");
-    //    if (!ModelState.IsValid)
-    //    {
-    //        ViewBag.Amount = amount;
-    //        return View(account);
-    //    }
-
-    //    // Note this code could be moved out of the controller, e.g., into the Model.
-    //    account.Balance += amount;
-    //    account.Transactions.Add(
-    //        new Transaction
-    //        {
-    //            TransactionType = "D",
-    //            Amount = amount,
-    //            TransactionTimeUtc = DateTime.UtcNow
-    //        });
-
-    //    await _context.SaveChangesAsync();
-
-    //    return RedirectToAction(nameof(Index));
-    //}
 }
