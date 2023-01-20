017524b2509d54f998150afd07e63bb56ad58fbe
Duc Vu <s3952670@student.rmit.edu.au>
2023-01-20 16:01:13 +1100

    statement is done

--- Controllers/TransactionController.cs
+++ Controllers/TransactionController.cs
@@ -4,11 +4,16 @@ using Assignment2.Filter;
 using Assignment2.Models;
 using Assignment2.Utilities;
 using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using X.PagedList;
+using Castle.Core.Resource;
+using Newtonsoft.Json;
 
 namespace Assignment2.Controllers;
 public class TransactionController : Controller
     {
     private readonly McbaContext _context;
+
+    private const string SessionKey_Account = "TransactionController_Account";
 
     public TransactionController(McbaContext context) => _context = context;
 
@@ -64,33 +69,15 @@ public class TransactionController : Controller
         return RedirectToAction("Index", "Customer");
     }
 
-    public async Task<IActionResult> Statement(int id) => View(await _context.Accounts.FindAsync(id));
 
-    //[HttpPost]
-    //public async Task<IActionResult> Statem(int id, decimal amount)
-    //{
-    //    var account = await _context.Accounts.FindAsync(id);
+    public async Task<IActionResult> Statement(int id, int? page = 1)
+    {
+        var Account = await _context.Accounts.FindAsync(id);
 
-    //    if (amount <= 0)
-    //        ModelState.AddModelError(nameof(amount), "Amount must be positive.");
-    //    if (amount.HasMoreThanTwoDecimalPlaces())
-    //        ModelState.AddModelError(nameof(amount), "Amount cannot have more than 2 decimal places.");
-    //    if (!ModelState.IsValid)
-    //    {
-    //        ViewBag.Amount = amount;
-    //        return View(account);
-    //    }
-
-    //    LogTransaction(account, -amount, "W");
-    //    if (account.Balance < 0)
-    //    {
-    //        ModelState.AddModelError(nameof(amount), "Insuffcient funds");
-    //        return View(account);
-    //    }
-    //    await _context.SaveChangesAsync();
-
-    //    return RedirectToAction("Index", "Customer");
-    //}
+        const int pageSize = 4;
+        var pagedList = await _context.Transactions.Where(x => x.AccountNumber == Account.AccountNumber).OrderByDescending(x => x.TransactionTimeUtc).ToPagedListAsync(page, pageSize);
+        return View(pagedList);
+    }
 
     private void LogTransaction(Account account, decimal amount, string type)
     {   
