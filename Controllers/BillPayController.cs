3446af2cfe097536e7078a1c49d1acbbbbd54947
Duc Vu <s3952670@student.rmit.edu.au>
2023-01-22 09:40:04 +1100

    BillPay view

--- Controllers/BillPayController.cs
+++ Controllers/BillPayController.cs
@@ -1,6 +1,19 @@
-﻿namespace Assignment2.Controllers
+﻿using Assignment2.Data;
+using Microsoft.AspNetCore.Mvc;
+namespace Assignment2.Controllers
 {
-    public class BillPayController
+    public class BillPayController : Controller
     {
+        private readonly McbaContext _context;
+
+        public BillPayController(McbaContext context) => _context = context;
+
+        public async Task<IActionResult> BillPay()
+        {
+            var test = _context.Payee.ToList();
+            foreach(var item in test)
+                Console.WriteLine(item);
+            return Ok();
+        }
     }
 }
