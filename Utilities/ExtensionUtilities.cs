1dea19e0599d25bf25897715900282169f9d37cd
Duc Vu <s3952670@student.rmit.edu.au>
2023-01-21 15:08:11 +1100

    .

--- Utilities/ExtensionUtilities.cs
+++ Utilities/ExtensionUtilities.cs
@@ -7,7 +7,7 @@ public static class MiscellaneousExtensionUtilities
     public static bool HasMoreThanNDecimalPlaces(this decimal value, int n) => decimal.Round(value, n) != value;
     public static bool HasMoreThanTwoDecimalPlaces(this decimal value) => value.HasMoreThanNDecimalPlaces(2);
 
-    public static IPagedList<Transaction> TimeConverter(IPagedList<Transaction> list)
+    public static IPagedList<Transaction> ConverterUtcTimeToLocalTime(IPagedList<Transaction> list)
     {
         foreach(var transaction in list)
         {
