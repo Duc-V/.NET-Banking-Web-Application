29d47851fa2a3ffd7cdfa869b68dd8c53db4c04c
zain-alshanan <s3947359@student.rmit.edu.au>
2023-01-24 22:39:13 +1100

    .

--- Controllers/CustomerController.cs
+++ Controllers/CustomerController.cs
@@ -3,38 +3,20 @@ using Assignment2.Data;
 using Assignment2.Filter;
 using Assignment2.Models;
 using Assignment2.Utilities;
-namespace Assignment2.Controllers;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using X.PagedList;
+using Castle.Core.Resource;
+using Newtonsoft.Json;
+using Microsoft.EntityFrameworkCore;
+using System.ComponentModel;
 
-// Can add authorize attribute to controllers.
-[AuthorizeCustomer]
-public class CustomerController : Controller
-{
+namespace Assignment2.Controllers;
+public class TransactionController : Controller
+    {
     private readonly McbaContext _context;
 
-    // ReSharper disable once PossibleInvalidOperationException
-    private int CustomerID => HttpContext.Session.GetInt32(nameof(Customer.CustomerID)).Value;
+   
 
-    public CustomerController(McbaContext context) => _context = context;
 
-    private Customer _customer;
-    //[AuthorizeCustomer]
-    public async Task<IActionResult> Index()
-    {
-        // Lazy loading.
-        // The Customer.Accounts property will be lazy loaded upon demand.
-        var customer = await _context.Customers.FindAsync(CustomerID);
-        _customer = customer;
-        // OR
-        // Eager loading.
-        //var customer = await _context.Customers.Include(x => x.Accounts).
-        //    FirstOrDefaultAsync(x => x.CustomerID == _customerID);
-
-        return View(customer);
-    }
-
-    public async Task<IActionResult> Menu(int id) {
-        HttpContext.Session.SetInt32("AccountNumber", id);
-        return View(await _context.Accounts.FindAsync(id));
-    }
 
 }
