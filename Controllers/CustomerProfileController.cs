b57c41fb5de14edba25401fe5a760dae69cb93d5
zain-alshanan <s3947359@student.rmit.edu.au>
2023-01-24 11:01:06 +1100

    correctly updating customer information

--- Controllers/CustomerProfileController.cs
+++ Controllers/CustomerProfileController.cs
@@ -2,110 +2,75 @@
 using Assignment2.Data;
 using Assignment2.Models;
 using SimpleHashing.Net;
+using Assignment2.Filter;
+using Microsoft.AspNetCore.Authorization;
+using System.Data;
+using System.ComponentModel.DataAnnotations;
+using Microsoft.EntityFrameworkCore;
 
-namespace Assignment2.Controllers
+namespace Assignment2.Controllers;
+
+// Can add authorize attribute to controllers.
+[AuthorizeCustomer]
+public class CustomerProfileController : Controller
 {
-    public class CustomerProfileController : Controller
+    private readonly McbaContext _context;
+    private readonly ISimpleHash _simpleHash;
+
+    // ReSharper disable once PossibleInvalidOperationException
+    private int CustomerID => HttpContext.Session.GetInt32(nameof(Customer.CustomerID)).Value;
+
+    public CustomerProfileController(McbaContext context, ISimpleHash simpleHash)
     {
-
-        private readonly McbaContext _context;
-        private readonly ISimpleHash _simpleHash;
-        public CustomerProfileController(McbaContext context, ISimpleHash simpleHash)
-        {
-            _context = context;
-            _simpleHash = simpleHash;
-        }
-
-        // GET request to view customer profile
-        public async Task<IActionResult> Index()
-        {
-            // Verify that the user is logged in
-            if (!HttpContext.Session.TryGetValue(nameof(Customer.CustomerID), out var customerIDValue))
-            {
-                return RedirectToAction("Login", "Login");
-            }
-
-            // Get the customer's information
-            var customerID = BitConverter.ToInt32(customerIDValue);
-            var customer = await _context.Customers.FindAsync(customerID);
-
-            // Display the customer's information in a view
-            return View(customer);
-        }
-
-        // POST request to save changes to customer information
-        [HttpPost]
-        public async Task<IActionResult> SaveChanges([FromForm] Customer updatedCustomer)
-        {
-            // Verify that the user is logged in
-            if (!HttpContext.Session.TryGetValue(nameof(Customer.CustomerID), out var customerIDValue))
-            {
-                return RedirectToAction("Login", "Login");
-            }
-            var customerID = BitConverter.ToInt32(customerIDValue);
-            var customer = await _context.Customers.FindAsync(customerID);
-
-            // Update the customer's information
-            customer.Name = updatedCustomer.Name;
-            customer.Address = updatedCustomer.Address;
-            customer.City = updatedCustomer.City;
-            customer.PostCode = updatedCustomer.PostCode;
-            _context.Customers.Update(customer);
-            await _context.SaveChangesAsync();
-
-            // Redirect the user back to the profile page
-            return RedirectToAction("Index");
-        }
-
-
-
-
-
-
-
-
-        [HttpGet]
-        public IActionResult ChangePassword()
-        {
-            return View("ChangePassword");
-        }
-
-        [HttpPost]
-        public async Task<IActionResult> ChangePassword(string currentPassword, string newPassword, string confirmNewPassword)
-        {
-            // Verify that the user is logged in
-            if (!HttpContext.Session.TryGetValue(nameof(Customer.CustomerID), out var customerIDValue))
-            {
-                return RedirectToAction("Login", "Login");
-            }
-            var customerID = BitConverter.ToInt32(customerIDValue);
-            var customer = await _context.Customers.FindAsync(customerID);
-
-            // Verify that the current password is correct
-            if (!_simpleHash.Verify(currentPassword, customer.Login.PasswordHash))
-            {
-                ModelState.AddModelError("CurrentPasswordIncorrect", "The current password is incorrect, please try again.");
-                return View("ChangePassword");
-            }
-
-            // Verify that the new password and confirmation match
-            if (newPassword != confirmNewPassword)
-            {
-                ModelState.AddModelError("NewPasswordMismatch", "The new password and confirmation do not match, please try again.");
-                return View("ChangePassword");
-            }
-
-            // Hash the new password
-            var newPasswordHash = _simpleHash.Compute(newPassword);
-            customer.Login.PasswordHash = newPasswordHash;
-            _context.Customers.Update(customer);
-            await _context.SaveChangesAsync();
-
-            // Redirect the user back to the profile page
-            return RedirectToAction("Index", "Customer");
-        }
-
-
-
+        _context = context;
+        _simpleHash = simpleHash;
     }
+
+    private Customer _customer;
+    //[AuthorizeCustomer]
+    public async Task<IActionResult> Index()
+    {
+        // Lazy loading.
+        // The Customer.Accounts property will be lazy loaded upon demand.
+        var customer = await _context.Customers.FindAsync(CustomerID);
+        _customer = customer;
+        // OR
+        // Eager loading.
+        //var customer = await _context.Customers.Include(x => x.Accounts).
+        //    FirstOrDefaultAsync(x => x.CustomerID == _customerID);
+
+        return View(customer);
+    }
+
+
+
+
+
+    [HttpPost]
+    public async Task<IActionResult> SaveChanges([Required, StringLength(50)] string name, [StringLength(50)] string address, [StringLength(40)] string city, [StringLength(4)] string postcode)
+    {
+        if (!ModelState.IsValid)
+        {
+            return BadRequest();
+        }
+
+        var customer = await _context.Customers.FindAsync(CustomerID);
+        _customer = customer;
+
+        // Update the customer's information
+        _customer.Name = name;
+        _customer.Address = address;
+        _customer.City = city;
+        _customer.PostCode = postcode;
+        _context.Customers.Update(_customer);
+        await _context.SaveChangesAsync();
+
+        // Redirect the user back to the profile page
+        return RedirectToAction("Index", "Customer");
+    }
+
 }
+
+
+
+
