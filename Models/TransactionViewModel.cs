1ccb24e2329751ffd4487e823c9008e037e2a511
Duc Vu <s3952670@student.rmit.edu.au>
2023-01-27 23:01:45 +1100

    added proper confimation page for widthdraw and deposit

---
+++ Models/TransactionViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Assignment2.Models;
+
+
+public class TransactionViewModel
+{   
+    public string TransactionType { get; set; }
+    public int AccountNumber { get; set; }
+    public string AccountType { get; set; }
+    [Required]
+    public decimal Amount { get; set; }
+
+    public string Comment { get; set; }
+    [Required]
+
+    public bool chargeFee { get; set; }
+    public int DestinationAccountNumber { get; set; }
+}
