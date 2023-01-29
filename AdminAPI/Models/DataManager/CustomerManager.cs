7362b26dbc790483823caf82500361eedacaa400
zain-alshanan <s3947359@student.rmit.edu.au>
2023-01-30 01:21:39 +1100

    reorganised file structure

---
+++ AdminAPI/Models/DataManager/CustomerManager.cs
@@ -0,0 +1,62 @@
+﻿using AdminAPI.Data;
+using AdminAPI.Models;
+using AdminAPI.Models.Repository;
+
+namespace AdminAPI.Models.DataManager;
+
+public class CustomerManager : ICustomerRepository<Customer, int>
+{
+    private readonly McbaContext _context;
+
+    public CustomerManager(McbaContext context)
+    {
+        _context = context;
+    }
+
+
+public IEnumerable<Customer> GetAll()
+{
+    return _context.Customers
+        .Select(c => new Customer
+        {
+            CustomerID = c.CustomerID,
+            Name = string.IsNullOrEmpty(c.Name) ? "null" : c.Name
+        })
+        .ToList();
+}
+
+    public Customer Get(int id)
+    {
+        var customer = _context.Customers
+            .Where(c => c.CustomerID == id)
+            .Select(c => new Customer
+            {
+                CustomerID = c.CustomerID,
+                Name = c.Name ?? "null",
+                Address = c.Address ?? "null",
+
+            })
+            .FirstOrDefault();
+
+        return customer;
+    }
+
+    public void UpdateCustomer(int id, Customer customer)
+    {
+        var customerToUpdate = _context.Customers.FirstOrDefault(c => c.CustomerID == id);
+
+        if (customerToUpdate != null)
+        {
+            customerToUpdate.Name = customer.Name;
+            customerToUpdate.TFN = customer.TFN;
+            customerToUpdate.Address = customer.Address;
+            customerToUpdate.City = customer.City;
+            customerToUpdate.PostCode = customer.PostCode;
+            customerToUpdate.Mobile = customer.Mobile;
+
+            _context.SaveChanges();
+        }
+    }
+
+
+}
---
+++ AdminAPI/Models/Repository/ICustomerRepository.cs
@@ -0,0 +1,15 @@
+﻿namespace AdminAPI.Models.Repository;
+
+public interface ICustomerRepository<TEntity, TKey> where TEntity : class
+{
+    // return all customers
+    IEnumerable<TEntity> GetAll();
+
+    // get individual customer from their id
+    Customer Get(int id);
+
+    // take in id and update respective customer.
+    void Update(TEntity entity, TKey id);
+
+
+}
