14401057edaef7bf222ccd0f16d5217972ddc926
Duc Vu <s3952670@student.rmit.edu.au>
2023-02-01 15:19:24 +1100

    merge

--- AdminWebsite/Controllers/BillPayController.cs
+++ AdminWebsite/Controllers/BillPayController.cs
@@ -5,12 +5,18 @@ using Newtonsoft.Json;
 namespace AdminWebsite.Controllers
 {
     public class BillPayController : Controller
-{
-    private readonly IHttpClientFactory _clientFactory;
-    private HttpClient Client => _clientFactory.CreateClient("api");
-
-    public BillPayController(IHttpClientFactory clientFactory) => _clientFactory = clientFactory;
-
     {
+        private readonly IHttpClientFactory _clientFactory;
+        private HttpClient Client => _clientFactory.CreateClient("api");
+
+        public BillPayController(IHttpClientFactory clientFactory) => _clientFactory = clientFactory;
+
+        public async Task<IActionResult> Index(int id)
+        {
+            
+        }
+
+
+
     }
 }
